3ef67ccc6d13a12d613a98bb9c24c011aa290ed2
LUIZ CELIO GOMES FILHO <luizceliogf@hotmail.com>
2023-10-11 16:25:50 -0300

    Registro funcionando com identity

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -10,12 +10,18 @@ namespace Polimedica.Controllers
     {
         private readonly UserManager<Pessoa> _userManager;
         private readonly SignInManager<Pessoa> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly PolimedicaDbContetxt _context;
 
-        public AccountController(UserManager<Pessoa> userManager, SignInManager<Pessoa> signInManager, PolimedicaDbContetxt context)
+        public AccountController(
+            UserManager<Pessoa> userManager,
+            SignInManager<Pessoa> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            PolimedicaDbContetxt context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
             _context = context;
         }
 
@@ -50,6 +56,38 @@ namespace Polimedica.Controllers
             }
             TempData["Error"] = "Wrong Credentials. Please, try again";
             return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterVM();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+            var user = await _userManager.FindByNameAsync(registerVM.Nome);
+            if(user == null)
+            {
+                var newUser = new Pessoa()
+                {
+                    PrimeiroNome = registerVM.Nome,
+                    funcao = registerVM.Funcao
+                };
+                var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Senha);
+                if(newUserResponse.Succeeded)
+                {
+                    var role = await _roleManager.RoleExistsAsync(registerVM.Funcao);
+                    if (!role) await _roleManager.CreateAsync(new IdentityRole(newUser.funcao));//Aqui eu poderia usar um data e deixar as funções previamente definidas(new IdentityRole(UserRolers.Gerente)) por exemplo
+                    await _userManager.AddToRoleAsync(newUser, newUser.funcao);//Aqui tinha que acompanhar a linha de cima
+                }
+                return RedirectToAction("Index", "Roteiro");//Alterar aqui para enviar para pagina de detalhes pessoal
+            }
+            return RedirectToAction("index", "home");
         }
     }
 }
---
+++ Data/UserRolers.cs
@@ -0,0 +1,10 @@
+﻿namespace Polimedica.Data
+{
+    public class UserRolers
+    {
+        //Essa classe está inutilizada, porem está configurada para funcionar em um futuro
+        public const string Gerente = "Gerente";
+        public const string Vendedor = "Vendedor";
+        public const string Outros = "Outros";
+    }
+}
